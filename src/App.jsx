6e8bb99088bdc4afc4e8c32f69d4bac505ba0956
saikumar <rakamsai3@gmail.com>
2025-04-22 17:33:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import'./index.css'
+import'./output.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='text-yellow-400'>sai</div>
+    </>
+  )
+}
+
+export default App
